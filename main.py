a2a84a5198a81656ed210c6907a4fdae844ea19b
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 13:36:52 +0530

    Make changes in main code to pass second testcase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def fibonacci(position):
+	if position < 0:
+        raise ValueError('Input should be postive')
 	if position == 1 or position == 2:
 		return 1
 	return fibonacci(position - 2) + 	fibonacci(position - 1)
