b007074cd2e0950ead4145fde8e0543ae6ed4d0d
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 13:29:39 +0530

    Add first testcase for checking fibonacci function

--- TestFibonacci.py
+++ TestFibonacci.py
@@ -1,4 +1,8 @@
 from unitest import TestCase
 
 class FibonacciTests(TestCase):
-	pass
+	def TestReturnsCorrectFibonacciNumber(self):
+		correctSequence = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+		for index in range(len(correctSequence)):
+			response = fibonacci(index)
+			assert response = correctSequence[index]
