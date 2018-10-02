7041866de2a0250ef2d1ef724fc792c83dd5b02e
Siva Prasad Raju <30493045+sivaprasadraju@users.noreply.github.com>
2018-10-02 15:35:54 +0530

    Refactor and Add testcase to check the length

--- TestFibonacci.py
+++ TestFibonacci.py
@@ -1,13 +1,20 @@
-from unittest import TestCase
-from main import fibonacci
+import unittest
+import pytest
+from main import *
 
-class FibonacciTests(TestCase):
-	def TestReturnsCorrectFibonacciNumber(self):
-		correctSequence = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
-		for index in range(len(correctSequence)):
-			response = fibonacci(index)
-			assert response = correctSequence[index]
-		
-	def TestRaiseValueErrorOnNegativeInput(self):
-		with pytest.raises(ValueError):
-			fibonacci(-1)
+class FibonacciTests(unittest.TestCase):
+
+    def test_fibonacci(self):
+        correct_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        response = fibonacci(13)
+        self.assertEquals(len(response), len(correct_sequence))
+
+    def test_fibonacciSeries(self):
+        correct_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        response = fibonacci(13)
+        for index in range(len(response)):
+            self.assertEquals(response[index], correct_sequence[index])
+
+    def test_negative(self):
+        with pytest.raises(ValueError):
+            fibonacci(-1)
